3471513389f8aa1aec10a2e0f5b1f2ce88a6d58d
pms <apark0907@ajou.ac.kr>
2023-05-17 22:09:03 +0900

    안쓰는 APIView 정리 & Response 형식 통일

--- users/views.py
+++ users/views.py
@@ -14,29 +14,6 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.core.mail import EmailMessage
 
 
-class Me(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        user = request.user
-        serializer = serializers.PrivateUserSerializer(user)
-        return Response(serializer.data)
-
-    def put(self, request):
-        user = request.user
-        serializer = serializers.PrivateUserSerializer(
-            user,
-            data=request.data,
-            partial=True,
-        )
-        if serializer.is_valid():
-            user = serializer.save()
-            serializer = serializers.PrivateUserSerializer(user)
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-
 class Users(APIView):  # 일반 유저 생성
     def post(self, request):
         password = request.data.get("password")
@@ -64,26 +41,9 @@ class Users(APIView):  # 일반 유저 생성
             email.send()
 
             serializer = serializers.UserSerializer(user)
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-
-class ChangePassword(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def put(self, request):
-        user = request.user
-        old_password = request.data.get("old_password")
-        new_password = request.data.get("new_password")
-        if not old_password or not new_password:
-            raise ParseError
-        if user.check_password(old_password):  # 예전 비번 확인
-            user.set_password(new_password)
-            user.save()
             return Response({"result": "OK", "status": 200})
         else:
-            raise ParseError
+            return {"result": "Forbidden", "status": 403}
 
 
 class LogIn(APIView):
@@ -106,41 +66,10 @@ class LogIn(APIView):
             return Response({"result": "Forbidden", "status": 403})
 
 
-class LogOut(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        logout(request)  # reuest 필수
-        return Response({"ok": "bye!"})
-
-
-class JWTLogIn(APIView):
-    def post(self, request):
-        username = request.data.get("username")
-        password = request.data.get("password")
-        if not username or not password:
-            raise ParseError
-        user = authenticate(
-            request,
-            username=username,
-            password=password,
-        )
-        if user:
-            token = jwt.encode(
-                {"pk": user.pk},
-                settings.SECRET_KEY,
-                algorithm="HS256",
-            )
-            return Response({"token": token})
-        else:
-            return Response({"error": "wrong password"})
-
-
 from rest_framework.exceptions import AuthenticationFailed
 
 
 class Activate(APIView):
-    # permission_classes = [IsAuthenticated]
     def post(self, request):
         token = request.data.get("Jwt")
         if not token:
@@ -152,14 +81,17 @@ class Activate(APIView):
         )
         pk = decoded.get("pk")
         if not pk:
-            raise AuthenticationFailed("Invalid Token")
+            return {"result": "Invalid Token", "status": 403}
+            # raise AuthenticationFailed("Invalid Token")
         try:
             user = User.objects.get(pk=pk)
             user.is_active = True
             user.save()
-            return Response({"ok": "good"})
+            return {"result": "Authentication PASS", "status": 200}
+            # return Response({"ok": "good"})
         except User.DoesNotExist:
-            raise AuthenticationFailed("User Not Found")
+            return {"result": "User Not Found", "status": 403}
+            # raise AuthenticationFailed("User Not Found")
 
 
 class is_email_available(APIView):
@@ -172,3 +104,73 @@ class is_email_available(APIView):
             return Response({"result": "possible email", "status": 200})
         except User.DoesNotExist:
             return Response({"result": "impossible email", "status": 403})
+
+
+# class ChangePassword(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def put(self, request):
+#         user = request.user
+#         old_password = request.data.get("old_password")
+#         new_password = request.data.get("new_password")
+#         if not old_password or not new_password:
+#             raise ParseError
+#         if user.check_password(old_password):  # 예전 비번 확인
+#             user.set_password(new_password)
+#             user.save()
+#             return Response({"result": "OK", "status": 200})
+#         else:
+#             raise ParseError
+
+
+# class LogOut(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def post(self, request):
+#         logout(request)  # reuest 필수
+#         return Response({"ok": "bye!"})
+
+
+# class JWTLogIn(APIView):
+#     def post(self, request):
+#         username = request.data.get("username")
+#         password = request.data.get("password")
+#         if not username or not password:
+#             raise ParseError
+#         user = authenticate(
+#             request,
+#             username=username,
+#             password=password,
+#         )
+#         if user:
+#             token = jwt.encode(
+#                 {"pk": user.pk},
+#                 settings.SECRET_KEY,
+#                 algorithm="HS256",
+#             )
+#             return Response({"token": token})
+#         else:
+#             return Response({"error": "wrong password"})
+
+
+# class Me(APIView):
+#     permission_classes = [IsAuthenticated]
+
+#     def get(self, request):
+#         user = request.user
+#         serializer = serializers.PrivateUserSerializer(user)
+#         return Response(serializer.data)
+
+#     def put(self, request):
+#         user = request.user
+#         serializer = serializers.PrivateUserSerializer(
+#             user,
+#             data=request.data,
+#             partial=True,
+#         )
+#         if serializer.is_valid():
+#             user = serializer.save()
+#             serializer = serializers.PrivateUserSerializer(user)
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
