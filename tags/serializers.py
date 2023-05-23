f0fc4403fd3052a94635d31528a415ea7e25048e
Pyoyeongjong <pyjong1999@gmail.com>
2023-05-23 15:40:01 +0900

    휴 살았다

--- tags/serializers.py
+++ tags/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Tag
-from wiki.models import Wiki
-from wiki.serializers import WikiSerializer
 from rest_framework import serializers
 
 class TagSerializers(ModelSerializer):
